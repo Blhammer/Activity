e6959a6269a4c91704c07f0195b11efe2db7de9e
Blhammer <60705901+Blhammer@users.noreply.github.com>
2022-10-25 23:53:45 +0300

    Created an Edit handler to update the profile data

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -1,5 +1,5 @@
-﻿using Application.Profiles;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Application.Profiles;
 
 namespace API.Controllers
 {
@@ -9,6 +9,12 @@ namespace API.Controllers
         public async Task<IActionResult> GetProfile(string username)
         {
             return HandleResult(await Mediator.Send(new Details.Query { Username = username }));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Edit(Edit.Command command)
+        {
+            return HandleResult(await Mediator.Send(command));
         }
     }
 }
