4180404f581b5f3fafb792726c5bc588d6330472
Blhammer <60705901+Blhammer@users.noreply.github.com>
2022-11-01 20:58:42 +0200

    App heroku deployment

--- client-app/src/app/common/form/MyTextArea.tsx
+++ client-app/src/app/common/form/MyTextArea.tsx
@@ -15,11 +15,11 @@ const MyTextArea = (props: Props) => {
         <Form.Field error={meta.touched && !!meta.error}>
             <label>{props.label}</label>
             <textarea {...field} {...props} />
-            {meta.touched && meta.error ? (
+            {meta.touched && meta.error && (
                 <Label basic color="red">
                     {meta.error}
                 </Label>
-            ) : null}
+            )}
         </Form.Field>
     );
 };
